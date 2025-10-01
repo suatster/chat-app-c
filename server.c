d5121f293357b4d71e84f39981cf95d6ef8e3597
suatster <bermaninho@gmail.com>
2025-10-01 23:56:42 +0300

    client can connect
    listen() and accept() done.

--- server.c
+++ server.c
@@ -14,19 +14,18 @@
 
 
 int main(){
+    /*SET SERVER*/
     //Open the socket
     int socketfd = socket(AF_INET, SOCK_STREAM, 0);
 
-    if(socketfd == -1){ // Failed?
-        fprintf(stderr, "Couldn't get socketfd.");
+    if(socketfd < 0){ // Failed?
+        perror("Couldn't get socketfd.");
         exit(EXIT_FAILURE);
     }
     printf("socketfd successful!\n");
 
-
     //Setting the structs
     struct sockaddr_in server_addr;
-
     memset(&server_addr, 0, sizeof(server_addr)); //reset the full struct
     server_addr.sin_family = AF_INET; //protocol, currently set to ipv4
     server_addr.sin_port = htons(PORT); //port to listen
@@ -40,6 +39,27 @@ int main(){
         exit(EXIT_FAILURE);
     }
     printf("Socket binding successful!\n");
-    
+
+
+    /*LISTEN*/
+    //time to listen, client is always right after all ¯\_(ツ)_/¯
+    if(listen(socketfd, 5) < 0){
+        //cant listen.
+        perror("Failed to listen!");
+        exit(EXIT_FAILURE);
+    }
+    printf("Listening on port %d.\n", PORT);
+
+
+    /*SET CLIENT*/
+    struct sockaddr_in client_addr;
+    socklen_t client_addr_size = sizeof(client_addr);
+    int clientfd = accept(socketfd, (struct sockaddr*)&client_addr, &client_addr_size);
+
+    if(clientfd < 0){ //Failed?
+        perror("accept() failed.");
+        exit(EXIT_FAILURE);
+    }
+    printf("Client connected!");
 
 }
